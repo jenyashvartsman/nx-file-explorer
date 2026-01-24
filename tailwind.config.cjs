80d2bb77424a0cfb907d13b8a3556e7536124422
Jenya Shvartsman <jenya.shvartsman@gmail.com>
2026-01-24 13:37:32 -0500

    feat: integrate Tailwind CSS with PostCSS and update styles

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './apps/web/src/**/*.{html,ts,scss}',
+    './apps/web/public/index.html',
+    './libs/**/src/**/*.{ts,html,scss}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
